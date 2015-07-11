7bd7211347ad170f0f744d684df9a363e8db2831
Nathan Dunfield <nathan@dunfield.info>
2015-07-11 13:27:17 -0700

    Moved most of __init__.py to a separate file

--- pe/fibrator.py
+++ pe/fibrator.py
@@ -1,6 +1,9 @@
 import os, time
 from gluing import GluingSystem
-from phc import PolyRing, PHCPoly, PHCSystem, ParametrizedSystem
+try:
+    from phc import PolyRing, PHCPoly, PHCSystem, ParametrizedSystem
+except ImportError:
+    print('No phc module, so will only work with precomputed fibers')
 from fiber import Fiber
 from snappy import Manifold, ManifoldHP
 
