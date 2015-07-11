34f953369205b736a59a46c8b3c246346f864e55
Nathan Dunfield <nathan@dunfield.info>
2015-07-11 12:39:17 -0700

    Making it possible to test all submodules at once

---
+++ pe/test.py
@@ -0,0 +1,11 @@
+import re, getopt, sys
+from . import sage_helper
+from . import complex_reps, real_reps, shape
+
+modules = [complex_reps, real_reps, shape]
+
+
+if __name__ == '__main__':
+    optlist, args = getopt.getopt(sys.argv[1:], 'v', ['verbose'])
+    verbose = len(optlist) > 0
+    sage_helper.doctest_modules(modules, verbose)
