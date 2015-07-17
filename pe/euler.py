c782f800c03701869a904ea0bac6342cd4ad3ddc
Marc Culler <culler@users.noreply.github.com>
2015-07-17 17:46:37 -0500

    Made pylint happier with euler.

--- pe/euler.py
+++ pe/euler.py
@@ -16,21 +16,21 @@ be defined in terms of the bar notation as
 That is cbar([g1|g2]) = s(g1 g2)^-1 s(g1) s(g2) rather
 that the RHS being cbar(g1, g2)
 """
-from .sage_helper import _within_sage, get_pi
+from .sage_helper import _within_sage
 if _within_sage:
-    from sage.all import ZZ, matrix, vector, sqrt, floor
-    Id2 = matrix(ZZ, [[1,0],[0,1]])
+    from sage.all import ZZ, matrix, sqrt
+    Id2 = matrix(ZZ, [[1, 0], [0, 1]])
 else:
-    from snappy.snap.utilities import Matrix2x2 as matrix, Vector2 as vector
-    Id2 = matrix(1,0,0,1)
+    from snappy.snap.utilities import Matrix2x2 as matrix
+    Id2 = matrix(1, 0, 0, 1)
 
 def wedge(a, b):
     return -a[0]*b[1] + a[1]*b[0]
 
 def orientation(a, b, c):
-    return cmp( wedge(a,b) * wedge(b,c) * wedge(c, a), 0)
+    return cmp(wedge(a, b) * wedge(b, c) * wedge(c, a), 0)
 
-class PointInP1R():
+class PointInP1R(object):
     """
     A point in P^1(R), modeled as a point on S^1 in R^2 whose polar
     angle satisfies 0 <= theta < pi.  We view R as the universal cover
@@ -45,17 +45,17 @@ class PointInP1R():
         if t != None:
             if t == 0:
                 R = t.parent()
-                v = (R(1), R(0))
+                self.v = (R(1), R(0))
             else:
                 t = t - t.floor()
                 x = 1 - 2*t
                 y = sqrt(1 - x**2)
-                v = (x, y)            
+                self.v = (x, y)
         else:
-            v = self.normalize(v)
-        self.v = v
+            self.v = self._normalize(v)
 
-    def normalize(self, v):
+    @staticmethod
+    def _normalize(v):
         a, b = v
         norm = sqrt(a**2 + b**2)
         a, b = a/norm, b/norm
@@ -66,13 +66,13 @@ class PointInP1R():
     def lift(self):
         """The "standard" lift of this point, which lies in [0,1)"""
         return (1-self.v[0])/2
-    
+
     def __repr__(self):
         return "<[%.5f:%.5f] in P^1(R)>" % self.v
 
     def __rmul__(self, mat):
         a, b = self.v
-        return PointInP1R( (mat[0][0]*a + mat[0][1]*b,  mat[1][0]*a + mat[1][1]*b) )
+        return PointInP1R((mat[0][0]*a + mat[0][1]*b, mat[1][0]*a + mat[1][1]*b))
 
     def __getitem__(self, i):
         return self.v[i]
@@ -86,7 +86,7 @@ def sigma_action(A, x):
     Return sigma_A(x)
     """
     R = x.parent()
-    p0, p1 = A*PointInP1R( (R(1), R(0)) ), A*PointInP1R(t=x)
+    p0, p1 = A*PointInP1R((R(1), R(0))), A*PointInP1R(t=x)
     a1 = p1.lift()
     b1 = a1 if p0[0] >= p1[0] else a1 + 1
     return x.floor() + b1
@@ -98,7 +98,7 @@ def eval_cocycle(A, B, AB, x):
     value = sigma_action(A, sigma_action(B, x)) - sigma_action(AB, x)
     rounded = value.round()
     return value, rounded
-    
+
 def univ_euler_cocycle(A, B, samples=3):
     """
     Evaluate the universal euler cocycle on [A | B], the class of (1,
@@ -107,7 +107,7 @@ def univ_euler_cocycle(A, B, samples=3):
     The universal euler cocycle represents the class in H^2(PSL(2,R))
     which corresponds to the central extension
     Z -> ~PSL(2,R) -> PSL(2,R) .
-    
+
     Here ~PSL(2,R) denotes the subgroup of Homeo+(R) consisting of all lifts
     of elements of PSL(2,R), viewed as a subgroup of Homeo+(S^1)
     """
@@ -127,7 +127,7 @@ def univ_euler_cocycle(A, B, samples=3):
     # And let's print something here to see if this ever happens:
     print 'Trying random samples!'
     data = set()
-    for n in xrange(samples):
+    for _ in xrange(samples):
         x = R.random_element()
         ans, rounded = eval_cocycle(A, B, AB, x)
         if abs(ans - rounded) < epsilon:
@@ -141,19 +141,18 @@ def my_matrix_norm(A):
     Sage converts entries to doubles, which can lead to a huge loss of
     precision here.
     """
-    return max( abs(e) for e in A.list() )
-    
+    return max(abs(e) for e in A.list())
+
 def is_almost_identity(A, tol=0.8):
-    # First a quick check to rule out most inputs.  
+    # First a quick check to rule out most inputs.
     if abs(A[0][1]) > 1e-10 or abs(A[1][0]) > 1e-10:
         return False
     RR = A.base_ring()
-    error = min( my_matrix_norm(A - Id2),
-                 my_matrix_norm(A + Id2))
+    error = min(my_matrix_norm(A - Id2), my_matrix_norm(A + Id2))
     epsilon = RR(2.0)**RR(-tol*RR.prec()).floor()
     return error <= epsilon
 
-class PSL2RtildeElement:
+class PSL2RtildeElement(object):
     """
     An element of the central extension of SL(2,R) with center Z which
     is determined by the universal euler cocycle.
@@ -165,13 +164,13 @@ class PSL2RtildeElement:
         return sigma_action(self.A, x) + self.s
 
     def inverse(self):
-        A, s = self.A, self.s
+        A = self.A
         Ainv = A.adjoint()
         return PSL2RtildeElement(Ainv, -self.s - univ_euler_cocycle(A, Ainv))
 
     def __mul__(self, other):
-        A, s, B, t= self.A, self.s, other.A, other.s
-        return PSL2RtildeElement(A*B, s + t + univ_euler_cocycle(A,B))
+        A, s, B, t = self.A, self.s, other.A, other.s
+        return PSL2RtildeElement(A*B, s + t + univ_euler_cocycle(A, B))
 
     def __repr__(self):
         A_entries = tuple(self.A.list())
@@ -183,7 +182,7 @@ class PSL2RtildeElement:
     def is_central(self):
         return is_almost_identity(self.A)
 
-class LiftedFreeGroupRep:
+class LiftedFreeGroupRep(object):
     """
     A representation of a free group into ~PSL(2,R).
     """
@@ -196,7 +195,7 @@ class LiftedFreeGroupRep:
             gen_images[g] = Atil
             gen_images[g.upper()] = Atil.inverse()
         self.gen_images = gen_images
- 
+
     def __call__(self, word):
         ims = self.gen_images
         ans = ims[word[0]]
@@ -204,8 +203,8 @@ class LiftedFreeGroupRep:
             ans = ans * ims[w]
         return ans
 
-# This is not currently used.
-def eval_thurston_cocycle(A, B, p, samples=None):
+# These are not currently used.
+def eval_thurston_cocycle(A, B, p):
     return orientation(p, A*p, (A*B)*p)
 
 def thurston_cocycle_of_relation(rho, rel):
