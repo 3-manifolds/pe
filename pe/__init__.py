99fb4e8038215afd7ab98943051e570c7bd349e3
Marc Culler <culler@users.noreply.github.com>
2015-07-20 17:30:09 -0500

    Renamed Shapes and PolishedShapes as ShapeSet and PolishedShapeSet. Added __getitem__ to PECharVariety: returns a Fiber or a ShapeSet, depending on the number of arguments.

--- pe/__init__.py
+++ pe/__init__.py
@@ -1,7 +1,7 @@
 from .pecharvar import PECharVariety
 from .complex_reps import PSL2CRepOf3ManifoldGroup
 from .real_reps import PSL2RRepOf3ManifoldGroup
-from .shape import Shapes, PolishedShapes
+from .shape import ShapeSet, PolishedShapeSet
 from pe.sage_helper import _within_sage
 if _within_sage:
     from .SL2R_lifting import SL2RLifter
