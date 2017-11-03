29cd79b6cec0a078cd8a35cf6f9f6aca25f0e285
Marc Culler <Marc Culler@localhost>
2017-11-03 16:48:04 -0500

    Get rid of the quad precision fft and make the multiprecision fft work with lengths that are multiples of 2 or 3.  Sizes 192 and 384 are handy.

--- pe/mpfft.py
+++ pe/mpfft.py
@@ -1,95 +1,102 @@
 import numpy, math
-from numpy import array, zeros, exp
-from sage.all import ComplexField, pi, I
+from numpy import array, zeros, exp, take
+from sage.all import ComplexField, pi, I, factor
 
-class DoubleFFT:
+class BaseFFT:
     """
-    A bare bones, recursive FFT using ordinary double precision
-    arithmetic.
+    Base class for a bare bones, recursive FFT.  Accepts array
+    lengths of the form 2^m*3^n.
     """
     def __init__(self, N):
-        assert 2**int(math.log(N, 2)) == N, 'N must be a power of 2.'
+        factors = set(int(f[0]) for f in factor(N))
+        assert factors.issubset(set((2,3))), 'N must be 2^m*3^n.'
         self.N = N
-        # The FFT world thinks that the unit circle is oriented clockwise.
-        self.roots = exp(array([-2*n*numpy.pi*1j/N for n in range(N)]))
-        self.iroots = exp(array([2*n*numpy.pi*1j/N for n in range(N)]))
-            
-    def _fft(self, A):
+        self._initialize()
+        
+    def _initialize(self):
+        # Subclasses override this method to compute roots of unity.
+        # Remember that the FFT world orients the unit circle clockwise.
+        pass
+    
+    def _zeros(self, k):
+        # Subclasses override this method to return an array of zeros. 
+        pass
+        
+    def _fft(self, A, invert):
+        # The only difference between the fft and the inverse fft
+        # is which generator is chosen for the group of roots of 1.
+        # This choice is determined by the boolean 'invert'.
         k, N = len(A), self.N
-        half, stride = N // 2, N // k
         if k == 1:
             return A
-        even = self._fft(A[0:k:2])
-        odd = self._fft(A[1:k:2])
-        result = zeros(k, dtype='complex')
-        result[:k//2] = even + self.roots[0:half:stride]*odd
-        result[k//2:] = even + self.roots[half:N:stride]*odd
-        return result
-
-    def _ifft(self, A):
-        k, N = len(A), self.N
-        half, stride = N // 2, N // k
-        if k == 1:
-            return A
-        even = self._ifft(A[0:k:2])
-        odd = self._ifft(A[1:k:2])
-        result = zeros(k, dtype='complex')
-        result[:k//2] = even + self.iroots[0:half:stride]*odd
-        result[k//2:] = even + self.iroots[half:N:stride]*odd
-        return result
+        stride = N // k
+        if invert:
+            zhat, z2hat = self.izhat, self.iz2hat
+        else:
+            zhat, z2hat = self.zhat, self.z2hat
+        result = self._zeros(k)
+        if k%2 == 0:
+            even = self._fft(A[0:k:2], invert)
+            odd = self._fft(A[1:k:2], invert)
+            result[:k//2] = even + zhat[0:N//2:stride]*odd
+            result[k//2:] = even + zhat[N//2:N:stride]*odd
+            return result
+        elif k%3 == 0:
+            zeroth = self._fft(A[0:k:3], invert)
+            oneth = self._fft(A[1:k:3], invert)
+            twoth = self._fft(A[2:k:3], invert)
+            result[:k//3] = (zeroth +
+                zhat[0:N//3:stride]*oneth +
+                z2hat[0:N//3:stride]*twoth)
+            result[k//3:2*k//3:] = (zeroth +
+                zhat[N//3:2*N//3:stride]*oneth +
+                z2hat[N//3:2*N//3:stride]*twoth)
+            result[2*k//3:] = (zeroth +
+                zhat[2*N//3::stride]*oneth +
+                z2hat[2*N//3::stride]*twoth)
+            return result
 
     def fft(self, A):
         assert len(A) == self.N
-        return self._fft(A)
+        return self._fft(A, False)
     
     def ifft(self, A):
         assert len(A) == self.N
-        return self._ifft(A)/self.N
+        return self._fft(A, True)/self.N
 
-class ComplexFFT:
+class DoubleFFT(BaseFFT):
+    """
+    A bare bones, recursive FFT using ordinary double precision
+    arithmetic.  Accepts array lengths of the form 2^m*3^n.
+    """
+        
+    def _initialize(self):
+        N = self.N
+        self.zhat = exp(array([-2*n*numpy.pi*1j/N for n in range(N)]))
+        self.izhat = exp(array([2*n*numpy.pi*1j/N for n in range(N)]))
+        self.z2hat = self.zhat.take(xrange(0, 2*N, 2), mode='wrap')
+        self.iz2hat = self.izhat.take(xrange(0, 2*N, 2), mode='wrap')
+        
+    def _zeros(self, k):
+        return zeros(k, dtype='complex')
+
+class ComplexFFT(BaseFFT):
     """
     A bare bones, recursive FFT using Sage's arbitrary precision complex
-    numbers.
+    numbers.   Accepts array lengths of the form 2^m*3^n.
     """
     def __init__(self, N, precision=256):
-        assert 2**int(math.log(N, 2)) == N, 'N must be a power of 2.'
-        self.N = N
         self.precision = precision
-        self.field = F = ComplexField(precision)
-        # The FFT world thinks that the unit circle is oriented clockwise.
-        self.roots = array([F((-2*k*pi*I/N).exp()) for k in range(N)],
+        BaseFFT.__init__(self, N)
+        
+    def _initialize(self):
+        N, F = self.N, ComplexField(self.precision) 
+        self.zhat = array([F((-2*k*pi*I/N).exp()) for k in range(N)],
                            dtype='O')
-        self.iroots = array([F((2*k*pi*I/N).exp()) for k in range(N)],
+        self.izhat = array([F((2*k*pi*I/N).exp()) for k in range(N)],
                             dtype='O')
-            
-    def _fft(self, A):
-        k, N = len(A), self.N
-        half, stride = N // 2, N // k
-        if k == 1:
-            return A
-        even = self._fft(A[0:k:2])
-        odd = self._fft(A[1:k:2])
-        result = zeros(k, dtype='O')
-        result[:k//2] = even + self.roots[0:half:stride]*odd
-        result[k//2:] = even + self.roots[half:N:stride]*odd
-        return result
+        self.z2hat = self.zhat.take(xrange(0, 2*N, 2), mode='wrap')
+        self.iz2hat = self.izhat.take(xrange(0, 2*N, 2), mode='wrap')
 
-    def _ifft(self, A):
-        k, N = len(A), self.N
-        half, stride = N // 2, N // k
-        if k == 1:
-            return A
-        even = self._ifft(A[0:k:2])
-        odd = self._ifft(A[1:k:2])
-        result = zeros(k, dtype='O')
-        result[:k//2] = even + self.iroots[0:half:stride]*odd
-        result[k//2:] = even + self.iroots[half:N:stride]*odd
-        return result
-
-    def fft(self, A):
-        assert len(A) == self.N
-        return self._fft(A)
-    
-    def ifft(self, A):
-        assert len(A) == self.N
-        return self._ifft(A)/self.N
+    def _zeros(self, k):
+        return zeros(k, dtype='O')
