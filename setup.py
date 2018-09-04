a7ac5578f0bf1b94467feb31b1d64c5ad4b7ca3e
Nathan Dunfield <nathan@dunfield.info>
2018-09-04 18:39:50 +0000

    Elevation: When encounter a degenerate shape (common with PRCharVar when the meridian holonomy is near 0 and one is converging to an ideal point), stop tracking rather than raise an exception.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup( name = 'pe',
-       version = '0.1.2',
+       version = '0.1.3',
        install_requires = ['snappy>=2.6.1'],
        dependency_links = [],
        packages = ['pe'],
